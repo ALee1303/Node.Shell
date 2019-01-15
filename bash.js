7a774b861394cd33e7c93cbb63367c6eb2bad115
Seoung_Yeon Lee <alexlee131005@gmail.com>
2019-01-15 11:39:40 -0600

    enable require by string input

--- bash.js
+++ bash.js
@@ -1,14 +1,13 @@
 //Output a prompt
 
 process.stdout.write('prompt > ');
-const callback = require('./pwd');
 
 //the stdin 'data' event fires after a user types in a line
 process.stdin.on('data', data => {
   const cmd = data.toString().trim();
-  if (cmd === 'pwd') {
-    process.stdout.write(callback);
-  } else process.stdout.write('You typed: ' + cmd);
+  const callback = require('./' + cmd); // ./pwd
+  callback();
+  //else process.stdout.write('You typed: ' + cmd);
 
   process.stdout.write('\nprompt > ');
 });
