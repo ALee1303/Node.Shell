f36fdb28b6198a28fc69b4de5e91e8f317c63bfa
Seoung_Yeon Lee <alexlee131005@gmail.com>
2019-01-15 12:02:17 -0600

    ls file not working

---
+++ ls.js
@@ -0,0 +1,8 @@
+module.exports = fs.readdir('./', 'utf8', (err, files) => {
+    if (err) {
+        throw err;
+    } else {
+        process.stdout.write(files.join('\n'));
+        process.stdout.write("prompt > ");
+    }
+})
